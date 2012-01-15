9b7c1c307dc377c17459235822bef7c7b9a248a3
scottlholmes <scottlholmes@gmail.com>
2012-01-14 22:04:18 -0500

    Initial commit of basically a blank Microsoft Lightswitch project.

---
+++ cleverledgerLS/Server/UserCode/ApplicationDataService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.LightSwitch;
+using Microsoft.LightSwitch.Security.Server;
+namespace LightSwitchApplication
+{
+    public partial class ApplicationDataService
+    {
+        partial void Principals_All_PreprocessQuery(ref IQueryable<Principal> query)
+        {
+            query = query
+                .OrderBy(a => a.PrincipalName);
+        }
+    }
+}
